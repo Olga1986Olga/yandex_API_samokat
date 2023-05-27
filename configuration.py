0eee6edb61202a34f00decaac1cb3653c0d0cf35
Olga1986Olga <132929209+Olga1986Olga@users.noreply.github.com>
2023-05-27 21:36:13 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://2fe6ab6e-89d7-4068-91ac-299da668b2fe.serverhub.praktikum-services.ru"
+CREATE_NEW_ORDER = "/api/v1/orders"
+GET_ORDER_BY_NUMBER = "/api/v1/orders/track?t={track_number}"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+        "firstName": "Narutoo",
+        "lastName": "Uchihaa",
+        "address": "Konoha, 142 apt.",
+        "metroStation": 4,
+        "phone": "+79112223322",
+        "rentTime": 5,
+        "deliveryDate": "2023-05-28",
+        "comment": "Saske, come back to Konoha",
+        "color": ["BLACK"]
+}
+
+headers = {'Content-Type': 'application/json'}
